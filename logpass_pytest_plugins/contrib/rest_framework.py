8e210d1b8700bfd7355df86bdbbdf8455f46da03
Łukasz Skarżyński <skarzynski_lukasz@protonmail.com>
2022-02-02 08:36:26 +0100

    fix: fix imports in rest_framework plugin

--- logpass_pytest_plugins/contrib/rest_framework.py
+++ logpass_pytest_plugins/contrib/rest_framework.py
@@ -1,18 +1,17 @@
 import pytest
 
-from rest_framework.test import (
-    APIClient,
-    APIRequestFactory,
-)
-
 
 @pytest.fixture
 def api_rf():
     """``APIRequestFactory`` instance."""
+    from rest_framework.test import APIRequestFactory  # noqa: WPS433
+
     return APIRequestFactory()
 
 
 @pytest.fixture
 def api_client():
     """``APIClient`` instance."""
+    from rest_framework.test import APIClient  # noqa: WPS433
+
     return APIClient()
