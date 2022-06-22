189c9ebf05f4453352497dd9539f9db6be3c9bb8
Łukasz Skarżyński <skarzynski_lukasz@protonmail.com>
2022-06-22 17:46:24 +0200

    chore: add config for conventional-changelog

---
+++ .conventional-changelog.js
@@ -0,0 +1,23 @@
+'use strict'
+const configFactory = require('conventional-changelog-conventionalcommits')
+
+module.exports = configFactory({
+    types: [
+        // custom types
+        { type: 'build', scope: 'deps', section: 'Dependencies' },
+        // Default `conventional-changelog-conventionalcommits` types
+        // Reference: https://github.com/conventional-changelog/conventional-changelog/blob/master/packages/conventional-changelog-conventionalcommits/writer-opts.js#L180
+        { type: 'feat', section: 'Features' },
+        { type: 'feature', section: 'Features' },
+        { type: 'fix', section: 'Bug Fixes' },
+        { type: 'perf', section: 'Performance Improvements' },
+        { type: 'revert', section: 'Reverts' },
+        { type: 'docs', section: 'Documentation' },
+        { type: 'style', section: 'Styles', hidden: true },
+        { type: 'chore', section: 'Miscellaneous Chores', hidden: true },
+        { type: 'refactor', section: 'Code Refactoring' },
+        { type: 'test', section: 'Tests', hidden: true },
+        { type: 'build', section: 'Build System' },
+        { type: 'ci', section: 'Continuous Integration', hidden: true },
+    ],
+})
